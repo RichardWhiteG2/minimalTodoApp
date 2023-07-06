c45532cd630be99f6ebf79dc327a1c309a9f5fa0
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-07-05 18:03:58 -0600

    Hide Completed

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,17 +1,61 @@
-import React from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Image, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native'
 import { TodosList } from '../components/TodosList'
 import { todosData } from '../data/todos'
 
-export const HomeScreen = () => {
+interface Todo {
+    id: number;
+    text: string;
+    isCompleted: boolean;
+    isToday: boolean;
+    hour: string;
+  }
+  
+  
+  export const HomeScreen = () => {
+    const [localData, setLocalData] = useState([] as Todo[]);
+    const [isHidden, setIsHidden] = useState(false);
+      // Ordenar los datos al inicializar el componente
+    const sortTodos = () => {
+        console.log('Se ejecuta sortTodos.')
+      const sorted = [...todosData].sort((a, b) => {
+        if (a.isCompleted && !b.isCompleted) {
+          return 1;
+        }
+        if (!a.isCompleted && b.isCompleted) {
+          return -1;
+        }
+        return 0;
+      });
+      setLocalData(sorted);
+    };
+    useEffect(() => {
+      sortTodos();
+    }, []);
+    const handleHidePress = ()=>{
+        if(isHidden){
+            setIsHidden(false)
+            sortTodos();
+            return;
+        }
+        setIsHidden(!isHidden)
+        setLocalData(localData.filter(todo => !todo.isCompleted))
+    } 
   return (
     <View style={styles.container}>
         <Image
             source={{uri: 'https://t4.ftcdn.net/jpg/04/88/57/27/240_F_488572706_o6TjVp3YNYVKUChLFWqNgGop7rcXl9mj.jpg'}}
             style={styles.pic}
         />
-        <Text style={styles.title}>Today</Text>
-        <TodosList todosData={todosData.filter(todo => todo.isToday)}/>
+        <View style={{flexDirection:'row', alignItems: 'center', justifyContent:'space-between'}}>
+            <Text style={styles.title}>Today</Text>
+            <TouchableOpacity
+                onPress={handleHidePress}
+            >
+                <Text style={{color: '#3478f6'}}>{isHidden ? "Show Completed": "Hide Completed"}</Text>
+            </TouchableOpacity>
+        </View>
+        <TodosList todosData={localData.filter(todo => todo.isToday)}/>
         <Text style={styles.title}>Tomorrow</Text>
         <TodosList todosData={todosData.filter(todo => !todo.isToday)}/>
     </View>
