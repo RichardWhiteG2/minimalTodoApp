b7472c64438ea966598221c8234b253fa58a0fda
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-07-12 15:35:00 -0600

    Deleting Todos

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,8 +1,11 @@
 
 import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { Checkbox } from './Checkbox';
 import moment from 'moment';
+import { useAppDispatch, useAppSelector } from '../hooks/hooks';
+import { deleteTodoReducer } from '../reducers/todosSlice';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface TodoProps {
     id: number;
@@ -20,8 +23,23 @@ export const Todo = ({
     hour,
 }: TodoProps) => {
   const [localHour, setLocalHour] = useState(new Date(hour));
+  const dispatch = useAppDispatch();
+  const todos = useAppSelector( state => state.todos.todos)
+  const handleDeleteTodo = async () => {
+    dispatch(deleteTodoReducer(id));  
+    try {
+      await AsyncStorage.setItem('@Todos', JSON.stringify(
+        todos.filter(todo => todo.id !== id)
+      ));
+      console.log('Todo deleted correctly');
+    } catch (e) {
+      console.log(e);
+    }
+  }
   return (
     <View style={styles.container}>
+      <View style={{flexDirection:'row', alignItems:'center'}}>
+      
         <Checkbox id={id} text={text} 
                     isCompleted={isCompleted} 
                     isToday={isToday}
@@ -38,16 +56,20 @@ export const Todo = ({
                 : styles.time
                 }>{moment(localHour).format('LT')}</Text>
         </View>
+        </View>
+        <TouchableOpacity  onPress={handleDeleteTodo}>
+         <Text style={{fontSize:16, color:"black"}}> 🆑</Text>
+        </TouchableOpacity>
     </View>
   )
-}
+} 
 
 const styles = StyleSheet.create({
     container: {
       marginBottom: 20,
       flexDirection: 'row',
       alignItems: 'center', 
-    //   justifyContent: 'space-between',
+      justifyContent: 'space-between',
     },
     text: {
       fontSize: 15,
